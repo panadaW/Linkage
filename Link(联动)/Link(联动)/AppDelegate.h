224c279dcc52eee3f6e260d075cba496492cbdb3
王明申 <1043978937@qq.com>
2015-11-08 15:29:22 +0800

    项目初始化

---
+++ Link(联动)/Link(联动)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Link(联动)
+//
+//  Created by 王明申 on 15/11/8.
+//  Copyright © 2015年 晨曦的Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Link(联动)/Link(联动)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Link(联动)
+//
+//  Created by 王明申 on 15/11/8.
+//  Copyright © 2015年 晨曦的Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
